b68446474283cba0f43189377b37795db212e20b
uvchik <uvchik.git@posteo.eu>
2020-08-06 15:27:10 +0200

    adapt code style

--- setup.py
+++ setup.py
@@ -18,60 +18,54 @@ from setuptools import setup
 def read(*names, **kwargs):
     with io.open(
         join(dirname(__file__), *names),
-        encoding=kwargs.get('encoding', 'utf8')
+        encoding=kwargs.get("encoding", "utf8"),
     ) as fh:
         return fh.read()
 
 
 setup(
-    name='scenario-builder',
-    version='v0.0.1',
-    license='MIT',
-    description='Tools to build scenario inputs from historical data or future assumptions',
-    long_description='%s\n%s' % (
-        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
-        re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
+    name="scenario-builder",
+    version="v0.0.1",
+    license="MIT",
+    description="Tools to build scenario inputs from historical data or future assumptions",
+    long_description="%s\n%s"
+    % (
+        re.compile("^.. start-badges.*^.. end-badges", re.M | re.S).sub(
+            "", read("README.rst")
+        ),
+        re.sub(":[a-z]+:`~?(.*?)`", r"``\1``", read("CHANGELOG.rst")),
     ),
-    author='reegis',
-    author_email='krien@uni-bremen.de',
-    url='https://github.com/reegis/scenario_builder',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    author="reegis",
+    author_email="krien@uni-bremen.de",
+    url="https://github.com/reegis/scenario_builder",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
-        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: Unix',
-        'Operating System :: POSIX',
-        'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        # uncomment if you test on these interpreters:
-        # 'Programming Language :: Python :: Implementation :: IronPython',
-        # 'Programming Language :: Python :: Implementation :: Jython',
-        # 'Programming Language :: Python :: Implementation :: Stackless',
-        'Topic :: Utilities',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Utilities",
     ],
     project_urls={
-        'Documentation': 'https://scenario_builder.readthedocs.io/',
-        'Changelog': 'https://scenario_builder.readthedocs.io/en/latest/changelog.html',
-        'Issue Tracker': 'https://github.com/reegis/scenario_builder/issues',
+        "Documentation": "https://scenario_builder.readthedocs.io/",
+        "Changelog": "https://scenario_builder.readthedocs.io/en/latest/changelog.html",
+        "Issue Tracker": "https://github.com/reegis/scenario_builder/issues",
     },
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    python_requires=">=3.6",
     install_requires=[
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
