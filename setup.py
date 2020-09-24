683d90da9660612bab1f0f4e239b15eb4649967c
beierd <beier@uni-bremen.de>
2020-09-24 10:03:24 +0200

    Updates setup requirements

--- setup.py
+++ setup.py
@@ -69,6 +69,8 @@ setup(
     ],
     python_requires=">=3.6",
     install_requires=[
+        "oemof.thermal",
+        "emobpy"
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
     extras_require={
